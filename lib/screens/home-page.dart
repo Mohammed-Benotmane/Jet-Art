f5fe393be4baba647f81aaddf3f86272a3c42d4e
mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-09-10 19:46:31 +0200

    - updating the design

--- lib/screens/home-page.dart
+++ lib/screens/home-page.dart
@@ -9,71 +9,125 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Stack(
+      body: Stack(
         children: <Widget>[
           Container(
-            decoration:
-            BoxDecoration(image: DecorationImage(image: AssetImage("assets/artback.jpg"), fit: BoxFit.cover)),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.filter_list,
-                      color: Colors.white,
-                      size: 30,
-                    )
-                  ],
-                ),
-                SizedBox(height: 50),
-                Text("JetArt", style: TextStyle(fontSize: 36, color: Colors.white)),
-                SizedBox(height: 10),
-                Text("An artist is not paid for his labor but for his Vision",
-                    style: TextStyle(fontSize: 14, color: Colors.white)),
-                SizedBox(height: 10),
-                Container(
-                  child: Material(
-                    borderRadius: BorderRadius.circular(25),
-                    elevation: 7,
-                    child: TextField(
-                      onChanged: (text) {},
-                      decoration: InputDecoration(
-                        focusColor: Colors.transparent,
-                        prefixIcon: Icon(Icons.search),
-                        hintText: "Search",
-                        fillColor: Colors.transparent,
-                        filled: true,
-                        enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.transparent),
-                            borderRadius: BorderRadius.circular(25)),
-                        focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.transparent),
-                            borderRadius: BorderRadius.circular(25)),
+            decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage("assets/artback.jpg"), fit: BoxFit.cover)),
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 10),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.filter_list,
+                        color: Colors.white,
+                        size: 30,
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 70),
+                  Text("JetArt", style: TextStyle(fontSize: 36, color: Colors.white)),
+                  SizedBox(height: 20),
+                  Text("An artist is not paid for his labor but for his Vision",
+                      style: TextStyle(fontSize: 14, color: Colors.white)),
+                  SizedBox(height: 40),
+                  Container(
+                    child: Material(
+                      borderRadius: BorderRadius.circular(25),
+                      elevation: 7,
+                      child: TextField(
+                        onChanged: (text) {},
+                        decoration: InputDecoration(
+                          focusColor: Colors.transparent,
+                          prefixIcon: Icon(Icons.search),
+                          hintText: "Search",
+                          fillColor: Colors.transparent,
+                          filled: true,
+                          enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.transparent),
+                              borderRadius: BorderRadius.circular(25)),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.transparent),
+                              borderRadius: BorderRadius.circular(25)),
+                        ),
                       ),
                     ),
+                    width: MediaQuery.of(context).size.width * .8,
                   ),
-                  width: MediaQuery.of(context).size.width * .8,
-                ),
-              ],
+                  SizedBox(height: 60),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          CircleAvatar(
+                            child: Icon(Icons.star,color: Colors.amberAccent,size: 30,),
+                            backgroundColor: Colors.white,
+                            radius: 30,
+                          ),
+                          SizedBox(height: 10),
+                          Text("Popular",style: TextStyle(color:Colors.white)),
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          CircleAvatar(
+                            child: Icon(Icons.star,color: Colors.amberAccent,size: 30,),
+                            backgroundColor: Colors.white,
+                            radius: 30,
+                          ),
+                          SizedBox(height: 10),
+                          Text("Popular",style: TextStyle(color:Colors.white)),
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          CircleAvatar(
+                            child: Icon(Icons.star,color: Colors.amberAccent,size: 30,),
+                            backgroundColor: Colors.white,
+                            radius: 30,
+                          ),
+                          SizedBox(height: 10),
+                          Text("Popular",style: TextStyle(color:Colors.white)),
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          CircleAvatar(
+                            child: Icon(Icons.star,color: Colors.amberAccent,size: 30,),
+                            backgroundColor: Colors.white,
+                            radius: 30,
+                          ),
+                          SizedBox(height: 10),
+                          Text("Popular",style: TextStyle(color:Colors.white)),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
           Container(
             child: DraggableScrollableSheet(
-              initialChildSize: 0.3,
-              minChildSize: 0.1,
-              maxChildSize: 0.95,
+              initialChildSize: 0.4,
+              minChildSize: 0.4,
+              maxChildSize: 0.85,
               builder: (BuildContext context, myscrollController) {
                 return Container(
-                  color: Colors.tealAccent[200],
+                  color: Colors.white,
                   child: ListView.builder(
                     controller: myscrollController,
                     itemCount: 25,
                     itemBuilder: (BuildContext context, int index) {
                       return ListTile(
                           title: Text(
-                            'Dish $index',
-                            style: TextStyle(color: Colors.black54),
-                          ));
+                        'Dish $index',
+                        style: TextStyle(color: Colors.black54),
+                      ));
                     },
                   ),
                 );
@@ -84,6 +138,4 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-
-
 }
