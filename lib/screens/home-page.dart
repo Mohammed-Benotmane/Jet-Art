1a7e91dd09b435698ac19f2d9b6c4f222e51c5f1
mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-09-10 15:35:03 +0200

    - trying to fix draggableScrollSheet

--- lib/screens/home-page.dart
+++ lib/screens/home-page.dart
@@ -21,53 +21,74 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
               SizedBox(height: 50),
-              Column(
-                children: <Widget>[
-                  Text("JetArt",style: TextStyle(fontSize: 36,color: Colors.white)),
-                  SizedBox(height: 10),
-                  Text("An artist is not paid for his labor but for his Vision",style: TextStyle(fontSize: 14,color: Colors.white)),
-                  SizedBox(height: 10),
-                  Container(
-                    child: Material(
-                      borderRadius: BorderRadius.circular(25),
-                      elevation: 7,
-                      child: TextField(
-                        onChanged: (text) {},
-                        decoration: InputDecoration(
-                          focusColor: Colors.transparent,
-                          prefixIcon: Icon(Icons.search),
-                          hintText: "Search",
-                          fillColor: Colors.transparent,
-                          filled: true,
-                          enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.transparent),
-                              borderRadius: BorderRadius.circular(25)),
-                          focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.transparent),
-                              borderRadius: BorderRadius.circular(25)),
+              Stack(children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Text("JetArt",style: TextStyle(fontSize: 36,color: Colors.white)),
+                        SizedBox(height: 10),
+                        Text("An artist is not paid for his labor but for his Vision",style: TextStyle(fontSize: 14,color: Colors.white)),
+                        SizedBox(height: 10),
+                        Container(
+                          child: Material(
+                            borderRadius: BorderRadius.circular(25),
+                            elevation: 7,
+                            child: TextField(
+                              onChanged: (text) {},
+                              decoration: InputDecoration(
+                                focusColor: Colors.transparent,
+                                prefixIcon: Icon(Icons.search),
+                                hintText: "Search",
+                                fillColor: Colors.transparent,
+                                filled: true,
+                                enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.transparent),
+                                    borderRadius: BorderRadius.circular(25)),
+                                focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.transparent),
+                                    borderRadius: BorderRadius.circular(25)),
+                              ),
+                            ),
+                          ),
+                          width: MediaQuery.of(context).size.width * .8,
                         ),
+                      ],
+                    ),
+                    SingleChildScrollView(
+                      child: Column(
+                        children: <Widget>[
+                          ListView(
+                            shrinkWrap: true,
+                            children: <Widget>[
+                              DraggableScrollableSheet(
+                                  initialChildSize: 0.75,
+                                  minChildSize: 0.75,
+                                  maxChildSize: 1,
+                                  builder: (context,scrollController){
+                                    return SingleChildScrollView(
+                                      controller: scrollController,
+                                      child: Column(
+                                        children: <Widget>[
+                                          SizedBox(height: 20),
+                                          Container(
+                                            width: 100,
+                                            height: 100,
+                                            color: Colors.white,
+                                          ),
+                                        ],
+                                      ),
+                                    );
+                                  }),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
-                    width: MediaQuery.of(context).size.width * .8,
-                  ),
-                ],
-              ),
-              Expanded(
-                child: DraggableScrollableSheet(
-                  initialChildSize: 0.4,
-                    minChildSize: 0.4,
-                    maxChildSize: 0.6,
-                    builder: (context,scrollController){
-                  return SingleChildScrollView(
-                    controller: scrollController,
-                    child: Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.white,
-                    ),
-                  );
-                }),
-              ),
+                  ],
+                ),
+                
+              ],),
             ],
           ),
         ),
