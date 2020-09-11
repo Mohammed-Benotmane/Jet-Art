5e09461f228deb0f91c44a85a15d11ed116e24fa
mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-09-12 01:49:34 +0200

    - adding font family to pubspec - changing Listtile to a column inside a row - updating detail page design

--- lib/screens/art_detail.dart
+++ lib/screens/art_detail.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:jetart/models/art.dart';
 
@@ -14,12 +15,23 @@ class _ArtDetailState extends State<ArtDetail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
         children: <Widget>[
           Expanded(
             flex: 2,
             child: Container(
-              decoration: BoxDecoration(image: DecorationImage(image: NetworkImage(widget.art.imageUrl),fit: BoxFit.cover)),
+              decoration: BoxDecoration(
+                  image: DecorationImage(image: NetworkImage(widget.art.imageUrl), fit: BoxFit.cover)),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 30, top: 40),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Icon(Icons.arrow_back, color: Colors.white, size: 30),
+                  ],
+                ),
+              ),
             ),
           ),
           Expanded(
@@ -29,15 +41,112 @@ class _ArtDetailState extends State<ArtDetail> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text(widget.art.title,style: TextStyle(fontSize: 35,fontWeight: FontWeight.w500)),
-                  SizedBox(height: 20),
-                  Text(widget.art.description,style: TextStyle(color: Colors.grey,fontWeight: FontWeight.w400)),
-                  SizedBox(height: 20),
+                  Text(widget.art.title, style: TextStyle(fontSize: 38, fontWeight: FontWeight.w500)),
+                  SizedBox(height: 30),
+                  Text(widget.art.description,
+                      style: TextStyle(fontSize: 16, color: Colors.grey, fontWeight: FontWeight.w400)),
+                  SizedBox(height: 60),
+                  Container(
+                    child: Row(
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 40,
+                          backgroundImage: NetworkImage(widget.art.artist.photoUrl),
+                        ),
+                        SizedBox(width: 15),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              widget.art.artist.name,
+                              style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500),
+                            ),
+                            SizedBox(height: 5),
+                            Text(
+                              "${widget.art.artist.followers}k Followers",
+                              style: TextStyle(fontWeight: FontWeight.w500,color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 60),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Material(
+                            borderRadius: BorderRadius.circular(50),
+                            elevation: 5,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(50),
+                              child: CircleAvatar(
+                                child: Icon(
+                                  Icons.star,
+                                  color: Colors.amberAccent,
+                                  size: 30,
+                                ),
+                                backgroundColor: Colors.white,
+                                radius: 30,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Text("Popular", style: TextStyle(color: Colors.grey.shade600)),
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Material(
+                            borderRadius: BorderRadius.circular(50),
+                            elevation: 5,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(50),
+                              child: CircleAvatar(
+                                child: Icon(
+                                  Icons.star,
+                                  color: Colors.amberAccent,
+                                  size: 30,
+                                ),
+                                backgroundColor: Colors.white,
+                                radius: 30,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Text("Popular", style: TextStyle(color: Colors.grey.shade600)),
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Material(
+                            borderRadius: BorderRadius.circular(50),
+                            elevation: 5,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(50),
+                              child: CircleAvatar(
+                                child: Icon(
+                                  Icons.star,
+                                  color: Colors.amberAccent,
+                                  size: 30,
+                                ),
+                                backgroundColor: Colors.white,
+                                radius: 30,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Text("Popular", style: TextStyle(color: Colors.grey.shade600)),
+                        ],
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
           ),
-
         ],
       ),
     );
